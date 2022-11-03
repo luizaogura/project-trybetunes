44b02daffb42acf79e35b394af12cd086fd0bb6d
Luiza Ogura <luizapogura@gmail.com>
2022-11-03 18:42:27 -0300

    rendering header component in pages

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header data-testid="header-component">
+        <h1>Carregando...</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
