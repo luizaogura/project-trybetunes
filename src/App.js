2af8c9f9df2df0ddb38858d35406687bf22cbb27
Luiza Ogura <luizapogura@gmail.com>
2022-11-03 15:06:48 -0300

    creating routes in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Route exact path="/" Component={ Login } />
+          <Route exact path="/search" Component={ Search } />
+          <Route exact path="/album/:id" Component={ Album } />
+          <Route exact path="/favorites" Component={ Favorites } />
+          <Route exact path="/profile" Component={ Profile } />
+          <Route exact path="/profile/edit" Component={ ProfileEdit } />
+          <Route exact path="*" Component={ NotFound } />
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
